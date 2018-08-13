558ed04bcdb4926423057c50c1e49d48bdf6153f
Mira Moufarrej <miramou@stanford.edu>
2018-08-13 11:25:55 -0700

    Added air gaps to prevent drips. Changed protocol to do 1 plate at a time since slurry solidifies quickly.

--- Opentrons/scripts/1_add_slurry_lysis_to_plate.py
+++ Opentrons/scripts/1_add_slurry_lysis_to_plate.py
@@ -1,5 +1,6 @@
 ##SCRIPT TO TRANSFER SLURRY + LYSIS BUFFER TO 48 well PLATE
 ##TIME TO RUN: ~8 minutes
+##TOTAL TIPS USED: 0.5 boxes
 
 from opentrons import robot, containers, instruments
 from setup import *
@@ -68,7 +69,7 @@ slurry =  create_container_instance(
 p1200_multi = instruments.Pipette(
 	axis='a',
 	min_volume=100,
-	max_volume=1200,
+	max_volume=1000,
 	trash_container=trash,
 	tip_racks=racks,
 	channels=8,
@@ -83,63 +84,67 @@ start=datetime.now()
 print("Step 1: Add lysis and slurry to plate")
 print("%s" % (start))
 
-src_row = 1
-p1200_multi.pick_up_tip()
+p1200_multi.start_at_tip(racks[0].rows("2"))
+
+src_row_lysis = 1
 
 dispense_vol = lysis_volume
 dispense_iters = 1
+air_gap_vol = 100
 
 if lysis_volume > 1000:
 	dispense_iters = math.ceil((lysis_volume/1000))
 	dispense_vol /= dispense_iters
+	air_gap_vol = 1000 - dispense_vol
 
 for i in range(2):
 #Just under 3 minutes per plate
-#Works well if added 220 mL lysis buffer + 1.2% B-met
+#Lysis: Works well if added 220 mL lysis buffer + 1.2% B-met
+#Slurry: Works well if add 12 mL slurry to column 1,2 of divided plate holder. Do so when instructed to.
+
+	robot.pause()
+	check = input("Add empty plate to %s. Add slurry to position %s. Switch lysis buffer. Seal filled plates while waiting. Press enter to continue. " % (plate_slots[i], i+1))
+	robot.resume()
 
 	loop_start = datetime.now()
+
+	count = 1
+	for dst_row in plates[i].rows():
+		#Separated transfer wrapper into distinct steps for further control on apsiration rate and movement
+		if count % 3 == 0 or count == 1:
+			p1200_multi.pick_up_tip()
+
+		p1200_multi.mix(3, 500, slurry.rows(str(src_row_slurry)))
+		p1200_multi.blow_out(slurry.rows(str(src_row_slurry)))
+		p1200_multi.aspirate(200, slurry.rows(str(src_row_slurry)).bottom(),rate=0.1)
+		p1200_multi.delay(3)
+		p1200_multi.aspirate(50, slurry.rows(str(src_row_slurry)).top(30), rate=1.0) #air gap
+		p1200_multi.dispense(200, dst_row.top(-12))
+		p1200_multi.blow_out(dst_row.top(-12))
+		
+		if count % 3 == 0:
+			p1200_multi.drop_tip()
+
+		count += 1
+
+	p1200_multi.pick_up_tip()
 
 	for dst_row in plates[i].rows():
 		for i in range(dispense_iters):
-			p1200_multi.aspirate(dispense_vol, lysis.rows(str(src_row)).bottom(), rate=1.0)
-			p1200_multi.delay(0.5)
-			p1200_multi.aspirate(100, lysis.rows(str(src_row)).top(30), rate=1.0) #air gap
+			p1200_multi.aspirate(dispense_vol, lysis.rows(str(src_row_lysis)).bottom(), rate=1.0)
+			p1200_multi.delay(2)
+			p1200_multi.aspirate(air_gap_vol, lysis.rows(str(src_row_lysis)).top(30), rate=1.0) #air gap
 			p1200_multi.dispense(dispense_vol, dst_row.bottom())
 			p1200_multi.blow_out(dst_row.bottom())
 
-		src_row += 1
-
-
-	print("Time for loop completion: %s" % (datetime.now() - loop_start))
-
-p1200_multi.drop_tip()
-
-robot.home()
-
-p1200_multi.pick_up_tip()
-src_row = 1
-
-for i in range(2):
-#Just about 3 minutes and 30 seconds per plate
-#Works well if add 12 mL slurry to column 1,2 of divided plate holder
-
-	loop_start = datetime.now()
-
-	for dst_row in plates[i].rows():
-		#Separated transfer wrapper into distinct steps for further control on apsiration rate and movement
-		p1200_multi.mix(3, 500, slurry.rows(str(src_row)))
-		p1200_multi.blow_out(slurry.rows(str(src_row)))
-		p1200_multi.aspirate(200, slurry.rows(str(src_row)).bottom(),rate=0.1)
-		p1200_multi.delay(3)
-		p1200_multi.aspirate(50, slurry.rows(str(src_row)).top(30), rate=1.0) #air gap
-		p1200_multi.dispense(200, dst_row.top(-12))
-		p1200_multi.blow_out(dst_row.top(-12))
+		src_row_lysis += 1
 	
-	src_row += 1
+	p1200_multi.drop_tip()
+
+	src_row_slurry += 1
+
 	print("Time for loop completion: %s" % (datetime.now() - loop_start))
+	robot.home()
 
-
-p1200_multi.drop_tip()
 
 print("Total time: %s" % (datetime.now()-start))
-robot.home()
